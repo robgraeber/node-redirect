7dccd9a535398595edf001986ea5bc2b11deb6cb
Rob Graeber <bgraeber@gmail.com>
2015-07-10 23:21:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ http.createServer(function (req, res) {
     res.writeHead(301, {"Location": process.env.REDIRECT_URL || "http://example.com"});
     res.end();
 }).listen(process.env.PORT || 8000);
+
 console.log('Express server listening on port ' + (process.env.PORT || 8000));
