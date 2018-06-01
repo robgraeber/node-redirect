55fe6981fc03f300b56d62f4f1132a0bb3fbed24
Rob Graeber <bgraeber@gmail.com>
2018-06-01 05:31:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function (req, res) {
-    res.writeHead(301, {"Location": process.env.REDIRECT_URL || "http://example.com"});
+    res.writeHead(307, {"Location": process.env.REDIRECT_URL || "http://example.com"});
     res.end();
 }).listen(process.env.PORT || 8000);
 
